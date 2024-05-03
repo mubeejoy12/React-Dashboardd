303b974bd405756eaaf09b7f584ad921471e30f2
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-05-03 10:40:35 +0100

    Modified app.jsx to new file like topbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-
 import "./App.css";
 import Topbar from "./assets/scenes/global/Topbar";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 function App() {
-  
+  const [theme, colorMode] = useMode();
 
   return (
-    <>
-      <div className="app">app</div>
-    </>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <mian className="content">
+            <Topbar />
+          </mian>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
