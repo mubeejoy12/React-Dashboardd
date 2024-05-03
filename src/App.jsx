55ce0b9fc369c84a84b56f373ab154dba8b5e51c
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-05-03 10:38:18 +0100

    chore: imported topbar, colormodecontext  and  usemode, theme provider so it can be reusuable

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 
 import "./App.css";
-
+import Topbar from "./assets/scenes/global/Topbar";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 function App() {
   
 
