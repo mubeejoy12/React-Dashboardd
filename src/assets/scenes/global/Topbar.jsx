ed872c9efb597a86c0ac32540b4b2844075025c8
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-05-10 22:58:47 +0100

    make some chnages in funtionality and designs

--- src/assets/scenes/global/Topbar.jsx
+++ src/assets/scenes/global/Topbar.jsx
@@ -1,56 +1,50 @@
-import React from "react";
-import { useContext } from "react";
 import { Box, IconButton, useTheme } from "@mui/material";
-import { ColorModeContext, tokens } from "../../../theme";
-import { InputBase } from "@mui/material";
-import LightModeOutlined from "@mui/icons-material/LightModeOutlined";
-import DarkModeOutlined from "@mui/icons-material/DarkModeOutlined";
-import NotificationsOutlined from "@mui/icons-material/NotificationsOutlined";
-import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import { useContext } from "react";
+import { tokens,ColorModeContext } from "../../../theme";
+import InputBase from "@mui/material/InputBase";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import SearchIcon from "@mui/icons-material/Search";
-import { SettingsOutlined } from "@mui/icons-material";
 
 const Topbar = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const colorMode = useContext(ColorModeContext);
+
   return (
-    <Box
-      display={"flex"}
-      p={2}
-      sx={{ alignItems: "center", justifyContent: "space-between" }}
-    >
-      {/* Search Bar */}
+    <Box display="flex" justifyContent="space-between" p={2}>
+      {/* SEARCH BAR */}
       <Box
-        display={"flex"}
-        backgroundColor={colors.primary[100]}
-        borderRadius={"3px"}
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
       >
-        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="search" />
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
         <IconButton type="button" sx={{ p: 1 }}>
           <SearchIcon />
         </IconButton>
       </Box>
 
       {/* ICONS */}
-      <Box display={"flex"}>
-        {/* implememnting the light and dark mode on the icon button */}
+      <Box display="flex">
         <IconButton onClick={colorMode.toggleColorMode}>
           {theme.palette.mode === "dark" ? (
-            <DarkModeOutlined />
+            <DarkModeOutlinedIcon />
           ) : (
-            <LightModeOutlined />
+            <LightModeOutlinedIcon />
           )}
         </IconButton>
-        {/* end */}
         <IconButton>
-          <NotificationsOutlined />
+          <NotificationsOutlinedIcon />
         </IconButton>
         <IconButton>
-          <SettingsOutlined />
+          <SettingsOutlinedIcon />
         </IconButton>
         <IconButton>
-          <PersonOutlineIcon />
+          <PersonOutlinedIcon />
         </IconButton>
       </Box>
     </Box>
