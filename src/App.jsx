76a4363da96a1167026cbbbdb90c04b36a95b941
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-05-10 23:03:41 +0100

    installed proside package to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import "./App.css";
-import Topbar from "./assets/scenes/global/Topbar";
 import { ColorModeContext, useMode } from "./theme";
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./assets/scenes/global/Topbar";
+import Dashboard from "./assets/scenes/Dashboard";
+// import Sidebar from "./scenes/global/Sidebar";
+// import Team from "./scenes/global/team";
+// import Invoices from "./scenes/global/invoices";
+// import Contacts from "./scenes/global/contacts";
+// import Bar from "./scenes/global/bar";
+// import Form from "./scenes/global/form";
+// import Line from "./scenes/global/line";
+// import Pie from "./scenes/global/pie";
+// import FAQ from "./scenes/global/Faq";
+// import Geography from "./scenes/global/geography";
+// import Calender from "./scenes/global/calender";
+import { Route, Routes } from "react-router-dom";
+import Sidebar from "./assets/scenes/global/Sidebar";
+import Team from "./assets/scenes/team";
+import Contact from "./assets/scenes/contacts"
+import Invoices from "./assets/scenes/invoices";
+
+
 function App() {
   const [theme, colorMode] = useMode();
 
@@ -10,9 +29,23 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <mian className="content">
+          <Sidebar />
+          <main className="content">
             <Topbar />
-          </mian>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team/>}/>
+              <Route path="/contacts" element={<Contact/>}/>
+              <Route path="/invoices" element={<Invoices/>}/>
+              {/* <Route path="form" element={<Form/>}/> */}
+              {/* <Route path="bar" element={<Bar/>}/> */}
+              {/* <Route path="pie" element={<Pie/>}/> */}
+              {/* <Route path="line" element={<Line/>}/> */}
+              {/* <Route path="faq" element={<FAQ/>}/> */}
+              {/* <Route path="geography" element={<Geography/>}/> */}
+              {/* <Route path="calender" element={<Calender/>}/> */}
+            </Routes>
+          </main>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
--- src/assets/scenes/dashboard/index.jsx
+++ src/assets/scenes/dashboard/index.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import Header from "../../components/Header";
+import { Box } from "@mui/material";
 
-const index = () => {
+const Dashboard = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Box m={"20px"}>
+      <Box
+        display={"flex"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+      >
+       
+        <Header title={"DASHBOARD"} subtitle={"Welcome to your dashboard"} />
+      </Box>
+    </Box>
+  );
+};
 
-export default index
+export default Dashboard;
