f1b874760170d6b31516fae8aba48592963c2acf
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-05-21 10:03:25 +0100

    uncommented route import component

--- src/App.jsx
+++ src/App.jsx
@@ -34,10 +34,10 @@ function App() {
             <Topbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              <Route path="/team" element={<Team/>}/>
-              <Route path="/contacts" element={<Contact/>}/>
-              <Route path="/invoices" element={<Invoices/>}/>
-              {/* <Route path="form" element={<Form/>}/> */}
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contact />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/calendar" element={<Calender/>}/>
               {/* <Route path="bar" element={<Bar/>}/> */}
               {/* <Route path="pie" element={<Pie/>}/> */}
               {/* <Route path="line" element={<Line/>}/> */}
