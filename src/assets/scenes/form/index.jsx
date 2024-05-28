66552e57fef045d3f41f074e539a5fdbd648f59e
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-05-28 23:33:46 +0100

    setting the  schemma for  the forrm components

--- src/assets/scenes/form/index.jsx
+++ src/assets/scenes/form/index.jsx
@@ -1,18 +1,15 @@
 import { Box, Button, TextField } from "@mui/material";
 import { Formik } from "formik";
 import * as yup from "yup";
-// import useMediaQuery from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import Header from "../../components/Header";
 
-
 const Form = () => {
-  //   const isNonMobile = useMediaQuery("(min-width:600px)");
+  const isNonMobile = useMediaQuery("(min-width:600px)");
 
   const handleFormSubmit = (values) => {
     console.log(values);
   };
-
-
 
   return (
     <Box m="20px">
@@ -129,6 +126,29 @@ const Form = () => {
       </Formik>
     </Box>
   );
+};
+
+const phoneRegExp =
+  /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
+
+const checkoutSchema = yup.object().shape({
+  firstName: yup.string().required("required"),
+  lastName: yup.string().required("required"),
+  email: yup.string().email("invalid email").required("required"),
+  contact: yup
+    .string()
+    .matches(phoneRegExp, "Phone number is not valid")
+    .required("required"),
+  address1: yup.string().required("required"),
+  address2: yup.string().required("required"),
+});
+const initialValues = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  contact: "",
+  address1: "",
+  address2: "",
 };
 
 export default Form;
