b447737514005f770e92c463eb0caace04d62b3e
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-05-28 20:56:41 +0100

    dahboard componenets: setting the grid template to fit in  other  component coming

---
+++ src/assets/scenes/Dashboard/index.jsx
@@ -0,0 +1,28 @@
+    <Box m="20px">
+      {/* HEADER */}
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+
+        <Box>
+          <Button
+            sx={{
+              backgroundColor: colors.blueAccent[700],
+              color: colors.grey[100],
+              fontSize: "14px",
+              fontWeight: "bold",
+              padding: "10px 20px",
+            }}
+          >
+            <DownloadOutlinedIcon sx={{ mr: "10px" }} />
+            Download Reports
+          </Button>
+        </Box>
+      </Box>
+
+      {/* GRID & CHARTS */}
+      <Box
+        display="grid"
+        gridTemplateColumns="repeat(12, 1fr)"
+        gridAutoRows="140px"
+        gap="10px"
+      >
